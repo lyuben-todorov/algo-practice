4a82d0d5d5be05e5f0031d8b7f3ba92ef3800df4
Lyuben Todorov <lyuben.todorov@gmail.com>
2019-11-17 18:29:30 +0100

    Monte Carlo approximation of pi

--- approximate_pi.py
+++ approximate_pi.py
@@ -1,2 +1,54 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib as matplotlib
+import random as rand
+import math as math
+from matplotlib.animation import FuncAnimation as animation
+
+
+fig, (ax1, ax2) = plt.subplots(2)
+
+xdata, ydata = [], []
+approxdata = []
+
+quadrant = plt.Circle((0, 0), 1, color='r', fill=False, lw=2)
+ax1.add_artist(quadrant)
+
+it = 0;
+ax2.axhline(3.14159265359, color = 'r',)
+
+def approx_pi():
+    global it
+    inside = 0
+    n = len(xdata)
+    for i in range(n):
+        if (math.sqrt(ydata[i]*ydata[i] + xdata[i]*xdata[i])) < 1:
+            inside += 1
+    approximation = 4*(inside/n)
+    approxdata.append(approximation)
+    ax2.clear()
+    ax2.axhline(3.14159265359, color = 'r',)
+    ax2.plot(approxdata, '-b')
+    #ax2.plot([it],[approximation], '-o', color = 'red')
+    plt.title(' n = ' + str(n) + ' PI = {0:.16f}' .format(approximation))
+    # print( str(it) + ". n = " + str(n) + " PI = "  + str(approximation))
+    # print(approximation)
+    # print('\n')
+    it+=1
+
+
+def animate(i):
+    framex, framey = [], []
+    for i in range(0, 100):
+        x = rand.random()
+        y = rand.random()
+        framex.append(x)
+        framey.append(y)
+    ax1.scatter(framex, framey, marker='.')
+    xdata.extend(framex)
+    ydata.extend(framey) 
+    approx_pi()
+
+ani = animation(fig, animate, interval=1, frames=500, repeat=False)
+
+
+plt.show()
